81cef8afe1ff048e746240adf36a7c106caa8590
rladldks1130 <rladldks1130@gmail.com>
2024-09-27 14:29:31 +0900

    feat: 홈화면 필요 이미지 불러옴

---
+++ mongyu_img.py
@@ -0,0 +1,18 @@
+import pygame
+
+# Home
+home_bg = pygame.image.load(r"mongyu_image\home_bg.jpg")
+
+# button
+setting_btn = pygame.transform.scale(pygame.image.load(r"C:\Python_Project\mongyu\몽유\mongyu_image\dlatl_setting_above.png"), (300, 70))
+save_btn = pygame.transform.scale(pygame.image.load(r"C:\Python_Project\mongyu\몽유\mongyu_image\dlatl_save_above.png"), (300, 70))
+new_btn = pygame.transform.scale(pygame.image.load(r"C:\Python_Project\mongyu\몽유\mongyu_image\dlatl_new_above.png"), (300, 70))
+
+# setting
+setting_bg = pygame.image.load(r"C:\Python_Project\mongyu\몽유\mongyu_image\임시_설정창.png")
+
+# save
+save_bg = pygame.image.load(r"mongyu_image\save_bg.jpg")
+
+# new
+new_bg = pygame.image.load(r"mongyu_image\new_bg.jpg")
